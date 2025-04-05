9da91ed715f6fb004dfa054147d6d45e49fbf740
Zill_Laiss <zill_laiss@proton.me>
2025-04-05 17:12:21 +0700

    konsole and helix

---
+++ .local/share/konsole/ZL.profile
@@ -0,0 +1,11 @@
+[Appearance]
+ColorScheme=Sweet
+Font=JetBrainsMono Nerd Font Mono,13,-1,5,400,0,0,0,0,0,0,0,0,0,0,1
+
+[General]
+Command=/usr/bin/zsh
+Name=ZL
+Parent=FALLBACK/
+
+[Terminal Features]
+BlinkingCursorEnabled=true
--- opts/helix_init.sh
+++ opts/helix_init.sh
@@ -3,3 +3,5 @@ go install github.com/go-delve/delve/cmd/dlv@latest
 go install golang.org/x/tools/cmd/goimports@latest                    
 go install github.com/nametake/golangci-lint-langserver@latest        
 go install github.com/golangci/golangci-lint/cmd/golangci-lint@latest
+
+npm install -g typescript typescript-language-server
