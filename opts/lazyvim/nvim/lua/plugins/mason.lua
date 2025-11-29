f026ef1e7e29ce3159f6221ae4541011b9272c58
Zill_Laiss <zill_laiss@proton.me>
2025-11-29 14:30:28 +0700

    lazyvim as opt

---
+++ opts/lazyvim/nvim/lua/plugins/mason.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    "mason-org/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "stylua",
+        "shellcheck",
+        "shfmt",
+        "flake8",
+        "vim",
+        "lua",
+        "vimdoc",
+        "html",
+        "css",
+        "markdown",
+        "markdown_inline",
+        "go",
+        "templ",
+        "zig",
+        "sql",
+        "javascript",
+      },
+    },
+  },
+}
---
+++ opts/lazyvim/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,21 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    ensure_installed = {
+      "bash",
+      "html",
+      "go",
+      "javascript",
+      "json",
+      "lua",
+      "markdown",
+      "markdown_inline",
+      "python",
+      "regex",
+      "tsx",
+      "typescript",
+      "vim",
+      "yaml",
+    },
+  },
+}
